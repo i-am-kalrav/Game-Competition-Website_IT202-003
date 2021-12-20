112988e24abc55cc6c0526e2b6129b0ced38882d
i-am-kalrav <kalravsrivastava@gmail.com>
2021-12-20 14:18:49 -0500

    to ask professor because the storing Scores is not working

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -6,7 +6,22 @@ if (!is_logged_in()) {
 ?>
 <h1>Home</h1>
 <h5>Welcome, <?php se(get_username()); ?>!</h5>
+<div class="row">
+    <div class="col-8">
+    </div>
+    <div class="col-4">
+        <?php
+        $duration = "week";
+        include(__DIR__ . "/../../partials/highscore_table.php");
 
+        $duration = "month";
+        include(__DIR__ . "/../../partials/highscore_table.php");
+
+        $duration = "lifetime";
+        include(__DIR__ . "/../../partials/highscore_table.php");
+        ?>
+    </div>
+</div>
 <?php
 require_once(__DIR__ . "/../../partials/flash.php");
 ?>
